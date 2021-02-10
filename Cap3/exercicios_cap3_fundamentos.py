6c22baa73b5ebc29d3c3ccd10025d91b040aa52d
Antonio Carlos Lemos Júnior <antoniocljBR@users.noreply.github.com>
2021-02-09 23:36:23 -0300

    Update exercicios_cap3_fundamentos.py
    Continuando os estudos do Cap 3

--- Cap3/exercicios_cap3_fundamentos.py
+++ Cap3/exercicios_cap3_fundamentos.py
@@ -31,19 +31,38 @@ for i in tupla:
 
 print(lista)
 # Exercício 4 - Crie uma sequência de números pares entre 100 e 150 e imprima na tela
-
+for i in range(100,150,2):
+    print(i)
 
 # Exercício 5 - Crie uma variável chamada temperatura e atribua o valor 40. Enquanto temperatura for maior que 35, 
 # imprima as temperaturas na tela
+temperatura = 40
 
+while temperatura > 35:
+    print(temperatura)
+    temperatura -= 1
 
 # Exercício 6 - Crie uma variável chamada contador = 0. Enquanto counter for menor que 100, imprima os valores na tela,
 # mas quando for encontrado o valor 23, interrompa a execução do programa
+contador = 0
 
-
+while contador < 100:
+    if contador == 23:
+        break
+    print(contador)
+    contador += 1
+    
 # Exercício 7 - Crie uma lista vazia e uma variável com valor 4. Enquanto o valor da variável for menor ou igual a 20, 
 # adicione à lista, apenas os valores pares e imprima a lista
+lista = []
+valor = 4
 
+while valor <= 20:
+    if valor % 2 == 0:
+        lista.append(valor)
+    valor += 1
+    
+print(lista)
 
 # Exercício 8 - Transforme o resultado desta função range em uma lista: range(5, 45, 2)
 nums = range(5, 45, 2)
