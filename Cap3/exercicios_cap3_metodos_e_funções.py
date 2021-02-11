1092172fc798ebf01ff36e18d64aec723ea61424
Antonio Carlos Lemos Júnior <antoniocljBR@users.noreply.github.com>
2021-02-10 22:46:22 -0300

    Update exercicios_cap3_metodos_e_funções.py
    Estudando método describe() do pandas

--- Cap3/exercicios_cap3_metodos_e_funções.py
+++ Cap3/exercicios_cap3_metodos_e_funções.py
@@ -82,7 +82,7 @@ print(dir(dicionario))
 # Abaixo você encontra a importação do Pandas, um dos principais pacotes Python para análise de dados.
 # Analise atentamente todos os métodos disponíveis. Um deles você vai usar no próximo exercício.
 import pandas as pd
-dir(pd)
+print(dir(pd))
 
 
 # ************* Desafio ************* (pesquise na documentação Python)
